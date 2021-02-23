d4745fc86895cf93eb84cdcdabb9ab070522f37d
bplsmolders <79159158+bplsmolders@users.noreply.github.com>
2021-02-23 19:26:24 +0100

    changed property names and added interval
    property names are now the same as instructed in the project.
    
    quote and background color changes each 5 seconds

--- js/script.js
+++ js/script.js
@@ -4,37 +4,37 @@ A Random Quote Generator
 ******************************************/
 
 /*  In the variable quotes you'll find a multidimensional array .
-    Each array holds at least an "quote" and "actor" property.
-    Additional an array can contain an "episode" or "year" property.
+    Each array holds at least an "quote" and "source" property.
+    Additional an array can contain an "citation" or "year" property.
 */
 let quotes = [
   {quote:'Whenever I’m sad, I stop being sad and be awesome instead.' ,
-  actor: 'Barney Stinson',
-  episode: 'Season 1, Episode 3',
+  source: 'Barney Stinson',
+  citation: 'Season 1, citation 3',
   year: 2005 },
   {quote:'It’s just, eventually we’re all gonna move on. It’s called growing up.' ,
-  actor: 'Lily Aldrin',
-  episode: 'Season 5, Episode 9',
+  source: 'Lily Aldrin',
+  citation: 'Season 5, citation 9',
   year: 2010 },
   {quote:'The future is scary, but you can’t just run back to the past because it’s familiar. Yes it’s tempting, but it’s a mistake.' ,
-  actor: 'Robin Scherbatsky',
-  episode: 'Season 2, Episode 17',
+  source: 'Robin Scherbatsky',
+  citation: 'Season 2, citation 17',
   year: 2006},
   {quote:'That cake. Best cake I ever had. Seriously, my stomach was like, ‘Hey bro, I don’t know what you’re eating cause I don’t have any eyes but it’s basically awesome, so keep sending it down Gullet Alley.' ,
-  actor: 'Marshall Eriksen',
-  episode: 'Season 7, Episode 1',
+  source: 'Marshall Eriksen',
+  citation: 'Season 7, citation 1',
   year: 2012},
   {quote:'I realized that I’m searching, searching for what I really want in life. And you know what? I have absolutely no idea what that is.' ,
-  actor: 'Barney Stinson',
-  episode: 'Season 9, Episode 1'},
+  source: 'Barney Stinson',
+  citation: 'Season 9, citation 1'},
   {quote:'We struggle so hard to hold on to these things that we know are gonna disappear eventually. And that’s really noble.' ,
-  actor: 'Lily Aldrin',
+  source: 'Lily Aldrin',
   year: 2006},
   {quote:'Because sometimes even if you know how something’s gonna end, that doesn’t mean you can’t enjoy the ride.' ,
-  actor: 'Ted Mosby',
-  episode: 'Season 4, Episode 12'},
+  source: 'Ted Mosby',
+  citation: 'Season 4, citation 12'},
   {quote:'A lie is just a great story that someone ruined with the truth.' ,
-  actor: 'Barney Stinson',
+  source: 'Barney Stinson',
   year: 2009},
 ]
 
@@ -45,19 +45,17 @@ function getRandomQuote(array) {
 }
 
 
-
-
-/*  This function  prints a quote and a actor property with every quote.
-    Where applicable the function prints properties episode and year. */
+/*  This function  prints a quote and a source property with every quote.
+    Where applicable the function prints properties citation and year. */
 function printQuote () {
   let htmlQuote = getRandomQuote(quotes)   // calls randomQuote and puts it in a variable.
   let htmlString = `
   <p class="quote">${htmlQuote.quote}</p>
-  <p class="source">${htmlQuote.actor}`; // sets up the htmlSting that needs to be printed and calls the values of properties quote and actor.
-  if (htmlQuote.episode !== undefined ){
+  <p class="source">${htmlQuote.source}`; // sets up the htmlSting that needs to be printed and calls the values of properties quote and source.
+  if (htmlQuote.citation !== undefined ){
   htmlString += `
-    <span class="citation">${htmlQuote.episode}</span>`
-  } // adds a episode property with text to htmlString if applicable.
+    <span class="citation">${htmlQuote.citation}</span>`
+  } // adds a citation property with text to htmlString if applicable.
   if (htmlQuote.year !== undefined ){
   htmlString += `
     <span class="citation">${htmlQuote.year}</span>`;
@@ -73,8 +71,17 @@ const btn = document.querySelector('button');
 function random(number) {
   return Math.floor(Math.random() * (number+1));
 }
+
+// changes queote each 5 seconds
+let intervalChangeQuote = setInterval(printQuote, 5000);
+// changes background color each 5 seconds
+let intervalChangeColor = setInterval(randomColor, 5000);
+
+
+// calls function everytime the button is clicked.
+btn.onclick = randomColor();
 // Function calls the random color and replaces the background color.
-btn.onclick = function() {
+function randomColor() {
   const rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
   document.body.style.backgroundColor = rndCol;
 }
